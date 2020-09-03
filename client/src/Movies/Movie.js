1238c51ed2d755a0eb671d03b01be48f7b28f117
karmaeiic <46303520+karmaeiic@users.noreply.github.com>
2020-09-02 17:16:31 -0700

    movie.js

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from "react-router-dom";
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
+  const params = useParams().id;
  
   useEffect(() => {
     const id = 1;
